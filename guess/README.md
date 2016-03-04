05c780fa59d4a8721e8495c4ae4fe492e74f1d25
Christian Romney <cromney@pointslope.com>
2016-03-04 15:26:59 -0500

    Added just enough to the guessing game lab
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- guess/README.md
+++ guess/README.md
@@ -1,16 +1,29 @@
 # guess
 
-FIXME: Write a one-line description of your library/project.
+This is a lab for a "Guess My Number" game. A partial implementation is already given. Your task is to complete the game.
 
-## Overview
+## Mechanics
 
-FIXME: Write a paragraph about the library/project and highlight its goals.
+When the game loads, the computer has generated a random integer between 1 and 10, inclusive. There is a number input box and a button provided for the user to guess the computer's chosen number. When the button is clicked, the user's current guess is evaluated. Currently, the evaluation code adds the user's latest guess to a vector containing the history of guesses, but does not make a determination if the user is correct. The following tasks should be attempted by the student:
+
+* Evaluate the guess to determine if the user has won
+* If the user's guess is correct:
+  * Display a message to the user so they know the won!
+  * Disable the button that allows them to continue guessing
+  * Display a button that gives the user the opportunity to Play Again
+    * If the Play Again button is clicked:
+      * Re-enable the guessing button
+      * Replace the number to be guessed
+      * Empty the vector of guesses
+      * Hide the Play Again button
+* Limit the number of guesses the user is allowed to make before declaring a loss and ending the game
+* Keep track of the "high score" (the lowest number of guesses resulting in a win)
 
 ## Setup
 
 To get an interactive development environment run:
 
-    lein figwheel
+    rlwrap lein figwheel
 
 and open your browser at [localhost:3449](http://localhost:3449/).
 This will auto compile and send all changes to the browser without the
@@ -32,8 +45,12 @@ To create a production build run:
 And open your browser in `resources/public/index.html`. You will not
 get live reloading, nor a REPL. 
 
+## Alternative for Emacs + Cider 
+
+This application is setup to work with cider-mode. Open **core.cljs** and type `C-c M-J` to launch both a Clojure REPL and ClojureScript REPL. This takes the place of running `lein figwheel` from the command line.
+
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2016 Point Slope, LLC
 
 Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
--- guess/src/guess/core.cljs
+++ guess/src/guess/core.cljs
@@ -1,23 +1,80 @@
 (ns guess.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  "This is the main namespace for the CLJS application"
+  (:require [reagent.core :as reagent :refer [atom]]
+            [goog.string :as str]))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+;; -== Application State ==- 
 
-;; define your app data so that it doesn't get over-written on reload
+(def app-state
+  "This atom contains the application's state"
+  (atom {:number (inc (rand-int 10))
+         :guesses []}))
 
-(defonce app-state (atom {:text "Hello world!"}))
+;; -== Utilities ==- 
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(defn valid-guess?
+  "Checks if the string contains a valid numeric guess"
+  [s]
+  (and (not (str/isEmpty s))
+       (str/isNumeric s)))
 
-(reagent/render-component [hello-world]
-                          (. js/document (getElementById "app")))
+;; -== Event Handlers ==-
+;;
+;; Event handlers should execute in response to user
+;; input. They are responsible for reacting to events
+;; and updating the state of the application.
 
+(defn evaluate-guess
+  "This function should evaluate a user's guess
+  and update the application state accordingly."
+  [guess]
+  (when (valid-guess? guess)
+    (let [g (js/parseInt guess)]
+      (swap! app-state update-in [:guesses] conj g))))
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+;; -== React Components ==- 
+;;
+;; React components display the current application
+;; state. They should not mutate that state directly,
+;; but rather should emit events upon user interaction.
+
+(defn app-title
+  "The title section of the application"
+  []
+  [:hgroup
+   [:h1 "Guess My Number"]
+   [:h2 "Choose a number between 1 and 10 inclusive"]])
+
+;; TASK:
+;; Create a "New Game" button component that will start a new guessing game
+;; by generating a new number to be guessed and replacing the current
+;; value in the application state.
+
+(defn guessing-box
+  "This component presents the user with controls to make their guesses"
+  []
+  [:div.guess
+   [:span (str "Guesses so far " (count (:guesses @app-state)))]
+   [:input {:id "guess" :type "number" :min 1 :max 10 :step 1}]
+   [:button {:type "button"
+             :onClick (fn [e] (evaluate-guess
+                              (.-value (.getElementById js/document "guess")))) } "Guess!"]])
+
+(defn root
+  "The root component in the tree"
+  []
+  [:main
+   [app-title]
+   [guessing-box]])
+
+;; -== Render the component tree to the DOM ==- 
+
+(reagent/render-component
+ [root]
+ (. js/document (getElementById "app")))
+
+;; a development-time hook that fires when Figwheel reloads the page
+
+(defn on-js-reload [])
