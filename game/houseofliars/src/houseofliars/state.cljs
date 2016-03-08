f9bde0919fb918e3d163f4e38416cf3706b7b16a
Adrian Martin <amartin@pointslope.com>
2016-03-07 22:20:46 -0500

    Changed the default number of turns

--- game/houseofliars/src/houseofliars/state.cljs
+++ game/houseofliars/src/houseofliars/state.cljs
@@ -2,26 +2,26 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [houseofliars.api :as api]))
 
-(def blank-app-state {:candidates []
-                      :score {:right 0
-                              :wrong 0}
-                      :quotes []
-                      :asked []
-                      :tracker {}
-                      :game-title "HOUSE OF LIARS"
-                      :animate {1 {:toggle false
-                                   :animate "shake"}
-                                2 {:toggle false
-                                    :animate "tada"}
-                                3 {:toggle false
-                                    :animate "bounce"}
-                                4 {:toggle false
-                                   :animate "jello"}
-                                5 {:toggle false
-                                   :animate "swing"}}
-                      :max-questions 5})
+(def initial-app-state {:candidates []
+                        :score {:right 0
+                                :wrong 0}
+                        :quotes []
+                        :asked []
+                        :tracker {}
+                        :game-title "HOUSE OF LIARS"
+                        :animate {1 {:toggle false
+                                     :animate "shake"}
+                                  2 {:toggle false
+                                     :animate "tada"}
+                                  3 {:toggle false
+                                     :animate "bounce"}
+                                  4 {:toggle false
+                                     :animate "jello"}
+                                  5 {:toggle false
+                                     :animate "swing"}}
+                        :max-questions 10})
 
-(def app-state (atom blank-app-state))
+(def app-state (atom initial-app-state))
 
 (defn candidate-success-handler
   "Handler fn passed to ajax call. Adds the candidates to the app-state"
@@ -42,5 +42,5 @@
 (defn reset-app-state!
   "Resets the app-state"
   []
-  (swap! app-state merge blank-app-state)
+  (swap! app-state merge initial-app-state)
   (initialize-app-state!))
